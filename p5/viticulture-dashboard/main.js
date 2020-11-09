6d0f89ebe88abef6bdfbd36fa3a87c621669e55c
ferronj <jferron22@gmail.com>
2020-11-09 10:16:52 -0800

    commit from windows
    some wordpress posts to update into new github pages site and my first
    effort at the weather dashboard to track growing degree days

---
+++ p5/viticulture-dashboard/main.js
@@ -0,0 +1,25 @@
+const api = {
+  key: "5e2a14e125968973dab9895ad4550880"
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+  if (evt.keyCode == 13) {
+    getResults(searchbox.value);
+    console.log(searchbox.value);
+  }
+}
+
+function getResults(query) {
+  fetch('${api.base}weather?q=${query}&units=metric&APPID=${api.key}')
+    .then(weather => {
+      return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults (weather) {
+  console.log(weather);
+}
---
+++ p5/viticulture-dashboard/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  var canvas = createCanvas(200, 200);
+  canvas.parent('sketch-div');
+  background(255);
+			}
+
+function draw() {
+  // put drawing code here
+}
